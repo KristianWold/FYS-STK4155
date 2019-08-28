ed1c6af437f2bcdf76d661e4493f924316de943c
KristianWold <kriswold@student.matnat.uio.no>
2019-08-28 20:09:29 +0200

    Excercises week 1

---
+++ Week1/ex2.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+#1)
+n = 1000
+
+x = np.random.rand(n,1)
+
+y = 5*x*x + np.random.randn(n,1)
+
+X = x**2
+
+b = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+
+print(f"numpy: {b}")
+
+x_lin = np.linspace(0, 1, 100)
+
+plt.plot(x, y, "o")
+plt.plot(x_lin, b[0]*x_lin**2)
+plt.show()
+
+
+#2)
+X_lin = x_lin[:, np.newaxis]**2
+linreg = LinearRegression(fit_intercept=False)
+coeff = linreg.fit(X,y)
+
+print(f"scikit: {linreg.coef_}")
+
+ypredict = linreg.predict(X_lin)
+
+plt.plot(x, y, "o")
+plt.plot(x_lin, ypredict)
+plt.show()
+
+#3)
+print(f"Mean squared error: {mean_squared_error(y, linreg.predict(X))}")
+print(f"R2 score: {r2_score(y, linreg.predict(X))}")
